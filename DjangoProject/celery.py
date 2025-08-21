edcdf05c5eae2c514cda8927e8ed6a8eeea13d95
Stratila Ruvim <ruvim.stratila@mail.ebs-integrator.com>
2025-08-21 11:30:13 +0300

    refactor: fix docker-compose file path in CI workflow and update Celery beat schedule configuration

--- DjangoProject/celery.py
+++ DjangoProject/celery.py
@@ -14,7 +14,7 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'top-20-tasks-report-every-week': {
         'task': 'apps.tasks.tasks.top_user_tasks',
-        'schedule': 10.0,  # crontab(minute='30', hour='8', day_of_week='1-5')
+        'schedule': crontab(minute='30', hour='8', day_of_week='1-5')
     },
 }
 app.conf.timezone = 'UTC'
