fc260189df33667dd8296c2cc621a04a60331282
Stratila Ruvim <ruvim.stratila@mail.ebs-integrator.com>
2025-08-22 11:00:41 +0300

    refactor: externalize sensitive settings to `.env` file and integrate `django-environ` for environment variable management

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -3,6 +3,7 @@ Django settings for DjangoProject project.
 """
 
 from pathlib import Path
+import environ
 
 # -----------------------------
 # BASE DIR
@@ -10,47 +11,57 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # -----------------------------
+# ENVIRONMENT
+# -----------------------------
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+# citim fișierul .env din rădăcina proiectului (lângă manage.py)
+environ.Env.read_env(BASE_DIR / ".env")
+
+# -----------------------------
 # SECURITY
 # -----------------------------
-SECRET_KEY = 'django-insecure-padp=i%lys6-s3-+%vwbja9rxsu$=7undv(8txc$b^j90h)=ta'
-DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'host.docker.internal']
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env("DEBUG", default=True)
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 # -----------------------------
 # EMAIL
 # -----------------------------
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
+EMAIL_BACKEND = env("EMAIL_BACKEND")
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_PORT")
 
 # -----------------------------
 # INSTALLED APPS
 # -----------------------------
 INSTALLED_APPS = [
     # Django
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
 
     # Third-party
-    'rest_framework',
-    'rest_framework_simplejwt',
-    'drf_spectacular',
-    'django_filters',
-    'django_minio_backend',
-    'django_elasticsearch_dsl',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.github',
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "drf_spectacular",
+    "django_filters",
+    "django_minio_backend",
+    "django_elasticsearch_dsl",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.github",
 
     # Local apps
-    'apps.tasks',
-    'apps.users',
+    "apps.tasks",
+    "apps.users",
 ]
 
 SITE_ID = 1
@@ -59,50 +70,50 @@ SITE_ID = 1
 # MIDDLEWARE
 # -----------------------------
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
-ROOT_URLCONF = 'DjangoProject.urls'
+ROOT_URLCONF = "DjangoProject.urls"
 
 # -----------------------------
 # TEMPLATES
 # -----------------------------
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'DjangoProject.wsgi.application'
+WSGI_APPLICATION = "DjangoProject.wsgi.application"
 
 # -----------------------------
 # DATABASE
 # -----------------------------
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django-db',
-        'USER': 'django-user',
-        'PASSWORD': 'passw0rd',
-        'HOST': 'db',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": env("DB_ENGINE"),
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
 
@@ -111,9 +122,11 @@ DATABASES = {
 # -----------------------------
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379/1",
-        "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
+        "BACKEND": env("CACHE_BACKEND"),
+        "LOCATION": env("CACHE_LOCATION"),
+        "OPTIONS": {
+            "CLIENT_CLASS": env("CACHE_CLIENT_CLASS", default="django_redis.client.DefaultClient"),
+        },
     }
 }
 
@@ -121,65 +134,65 @@ CACHES = {
 # AUTHENTICATION
 # -----------------------------
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
 SOCIALACCOUNT_PROVIDERS = {
-    'github': {
-        'APP': {
-            'client_id': 'Ov23lia487wKwwf5GLlE',
-            'secret': '88bc004e9b0bb5c5499580f8e9a68565b0345d23',
-            'key': ''
+    "github": {
+        "APP": {
+            "client_id": env("GITHUB_CLIENT_ID"),
+            "secret": env("GITHUB_SECRET"),
+            "key": env("GITHUB_KEY"),
         }
     }
 }
 
-LOGIN_REDIRECT_URL = '/accounts/profile/'
+LOGIN_REDIRECT_URL = "/accounts/profile/"
 
 # -----------------------------
 # REST FRAMEWORK
 # -----------------------------
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    'DEFAULT_FILTER_BACKENDS': (
-        'django_filters.rest_framework.DjangoFilterBackend',
-        'rest_framework.filters.OrderingFilter',
-        'rest_framework.filters.SearchFilter',
+    "DEFAULT_FILTER_BACKENDS": (
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter",
+        "rest_framework.filters.SearchFilter",
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10,
 }
 
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'Task Managment System API',
-    'DESCRIPTION': 'A simple API for managing tasks and users',
-    'VERSION': '1.0.0',
+    "TITLE": "Task Managment System API",
+    "DESCRIPTION": "A simple API for managing tasks and users",
+    "VERSION": "1.0.0",
 }
 
 # -----------------------------
 # ELASTICSEARCH
 # -----------------------------
 ELASTICSEARCH_DSL = {
-    'default': {'hosts': 'http://elasticsearch:9200'}
+    "default": {"hosts": env("ELASTICSEARCH_HOST")},
 }
 
 # -----------------------------
 # STATIC FILES
 # -----------------------------
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'django.core.files.storage.FileSystemStorage'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.core.files.storage.FileSystemStorage"
 
 # -----------------------------
 # MEDIA FILES (MinIO)
@@ -201,32 +214,34 @@ STORAGES = {
 }
 DEFAULT_FILE_STORAGE = "django_minio_backend.models.MinioBackend"
 
-MINIO_ENDPOINT = 'minio:9000'
-MINIO_ACCESS_KEY = 'minio_user'
-MINIO_SECRET_KEY = 'minio_password'
-MINIO_USE_HTTPS = False
-MINIO_PUBLIC_BUCKETS = ['django-backend-dev-public']
-MINIO_CONSISTENCY_CHECK_ON_START = False
+MINIO_ENDPOINT = env("MINIO_ENDPOINT")
+MINIO_ACCESS_KEY = env("MINIO_ACCESS_KEY")
+MINIO_SECRET_KEY = env("MINIO_SECRET_KEY")
+MINIO_USE_HTTPS = env("MINIO_USE_HTTPS", default=False)
+MINIO_PUBLIC_BUCKETS = env.list("MINIO_PUBLIC_BUCKETS")
+MINIO_CONSISTENCY_CHECK_ON_START = env.bool(
+    "MINIO_CONSISTENCY_CHECK_ON_START", default=False
+)
 
 # -----------------------------
 # CELERY
 # -----------------------------
-CELERY_BROKER_URL = 'amqp://rabbit_user:rabbit_password@rabbitmq:5672//'
-CELERY_TIMEZONE = "Europe/Chisinau"
-CELERY_TASK_TRACK_STARTED = True
-CELERY_TASK_TIME_LIMIT = 30 * 60
-CELERY_RESULT_BACKEND = "redis://redis:6379/2"
-CELERY_TASKS_ALWAYS_EAGER = True
+CELERY_BROKER_URL = env("CELERY_BROKER_URL")
+CELERY_TIMEZONE = env("CELERY_TIMEZONE")
+CELERY_TASK_TRACK_STARTED = env.bool("CELERY_TASK_TRACK_STARTED", default=True)
+CELERY_TASK_TIME_LIMIT = env.int("CELERY_TASK_TIME_LIMIT", default=30 * 60)
+CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND")
+CELERY_TASKS_ALWAYS_EAGER = env.bool("CELERY_TASKS_ALWAYS_EAGER", default=True)
 
 # -----------------------------
 # INTERNATIONALIZATION
 # -----------------------------
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
 # -----------------------------
 # DEFAULT PK FIELD
 # -----------------------------
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
