8467c2bfc1f2e18c974224a9732c2b42f4e9dac7
Stratila Ruvim <ruvim.stratila@mail.ebs-integrator.com>
2025-08-25 14:49:37 +0300

    refactor: remove redundant logging and update email host configuration in `.env`

--- apps/tasks/receivers.py
+++ apps/tasks/receivers.py
@@ -11,18 +11,15 @@ logger = logging.getLogger(__name__)
 
 @receiver(signal=task_completed, sender=Task)
 def task_completed_email_handler(sender, instance, **kwargs):
-    logger.info("Task completed email handler")
     task_completed_email.delay(instance.id)
 
 
 @receiver(signal=task_commented, sender=Task)
 def task_commented_email_handler(sender, instance, **kwargs):
-    logger.info("Task commented email handler")
     comment = kwargs.get("comment", "")
     task_commented_email.delay(instance.id, comment=comment)
 
 
 @receiver(signal=task_assigned, sender=Task)
 def task_assigned_email_handler(sender, instance, **kwargs):
-    logger.info("Task assigned email handler")
     user_assigned_to_task_email.delay(instance.id)
